e704ecaadc504d1a8580379db7268cefddb98ce7
吳 蕥綺 <n0975116268@gmail.com>
2023-12-01 15:59:08 +0800

    Controller Exception統一處理

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -5,8 +5,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
-    @RequestMapping("/test")
-    public String test(){
-        return "hello world";
+    @RequestMapping("/test1")
+    public String test1(){
+        throw new RuntimeException("test1 error");
     }
+
+    @RequestMapping("/test2")
+    public String test2() {
+        throw new IllegalArgumentException("test2 error");
+    }
+
 }
