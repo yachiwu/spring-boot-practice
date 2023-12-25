6e4b607e0aaae94e0cbcd0989a99ef6f2b1c001d
吳 蕥綺 <n0975116268@gmail.com>
2023-12-25 15:04:54 +0800

    Introduce how to add log

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Student;
 import com.example.demo.service.StudentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import java.util.List;
 
 @RestController
 public class StudentController {
+    private final static Logger log = LoggerFactory.getLogger(StudentController.class);
     @Autowired
     private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
@@ -50,6 +53,7 @@ public class StudentController {
     }
     @GetMapping("students/{studentId}")
     public ResponseEntity<Student> selectStudent(@PathVariable Integer studentId){
+        log.info("get student {}",studentId);
         Student student = studentService.getByStudentId(studentId);
         return ResponseEntity.status(HttpStatus.OK).body(student);
     }
